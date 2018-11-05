70c05821220a82c2cc5618a674018a3df18814ad
Lee <lehady23@gmail.com>
2018-11-05 14:33:43 +0100

    New version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', loadChildren: 'app/areas/customers/customer.module#CustomersModule' },
+      { path: 'admin', loadChildren: 'app/areas/admin/admin.module#AdminModule' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
